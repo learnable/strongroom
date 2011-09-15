a9faa6b379bca01598a5efe89e6a8eb0140cc4c2
Paul Annesley <paul@annesley.cc>
2011-09-15 17:26:16 +1000

    Empty Strongroom module and minitest spec.

---
+++ lib/strongroom.rb
@@ -0,0 +1,2 @@
+module Strongroom
+end
---
+++ spec/strongroom_spec.rb
@@ -0,0 +1,9 @@
+require "pathname"
+$LOAD_PATH.unshift Pathname(__FILE__).dirname.parent.join("lib").to_path
+
+require "minitest/autorun"
+
+require "strongroom"
+
+describe Strongroom do
+end
