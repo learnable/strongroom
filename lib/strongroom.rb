82d4ae7826cfdd589bda03990c6b466ba401b777
Paul Annesley <paul@annesley.cc>
2011-09-15 22:51:50 +1000

    Strongroom facade: Strongroom.new.encryptor(pub_key).encrypt("secret") etc

--- lib/strongroom.rb
+++ lib/strongroom.rb
@@ -1,5 +1,27 @@
-require "strongroom/encryptor"
-require "strongroom/enigma"
+require "openssl"
+
+require_relative "strongroom/enigma"
+require_relative "strongroom/encryptor"
+require_relative "strongroom/decryptor"
 
 class Strongroom
+
+  def initialize cipher = nil
+    @cipher = cipher
+  end
+
+  def encryptor public_key
+    Encryptor.new cipher, public_key
+  end
+
+  def decryptor private_key
+    Decryptor.new cipher, private_key
+  end
+
+  private
+
+  def cipher
+    @cipher ||= OpenSSL::Cipher.new("AES-128-CFB")
+  end
+
 end
--- spec/strongroom_spec.rb
+++ spec/strongroom_spec.rb
@@ -2,5 +2,33 @@ require_relative "spec_helper"
 
 describe Strongroom do
 
+  describe "round trip encryption" do
 
+    # generate a key pair in memory
+    def key_pair
+      Struct.new(:private, :public).new(
+        pk = OpenSSL::PKey::RSA.new(1024),
+        pk.public_key
+      )
+    end
+
+    { short: 16, medium: 16 * 1024, long: 2**20 }.each do |name, length|
+
+      describe "with #{name} input (#{length} bytes)" do
+        before do
+          @input = "abcd1234" * (length / 8)
+          @input.length.must_equal length
+        end
+
+        it "works" do
+          key = key_pair
+          enigma = Strongroom.new.encryptor(key.public).encrypt(@input)
+          output = Strongroom.new.decryptor(key.private).decrypt(enigma)
+          output.must_equal @input
+        end
+      end
+
+    end
+
+  end
 end
