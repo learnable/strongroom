a315ae99f3a7c7e9a5951836694d97e7f3f3fdc2
Paul Annesley <paul@annesley.cc>
2013-03-01 01:43:58 +1100

    Refactored strongroom_enigma_spec to avoid #tap.

--- spec/strongroom_enigma_spec.rb
+++ spec/strongroom_enigma_spec.rb
@@ -35,39 +35,38 @@ describe Strongroom::Enigma do
 
   describe "#to_hash" do
     it "has string keys, binary values base64 encoded" do
-      enigma.to_hash.tap do |h|
-        h["cipher"].must_equal "AES-128-TEST"
-        h["ciphertext"].must_equal bin64("ct")
-        h["encrypted_key"].must_equal bin64("ek")
-        h["iv"].must_equal bin64("iv")
-      end
+      h = enigma.to_hash
+      h["cipher"].must_equal "AES-128-TEST"
+      h["ciphertext"].must_equal bin64("ct")
+      h["encrypted_key"].must_equal bin64("ek")
+      h["iv"].must_equal bin64("iv")
     end
     it "has US-ASCII keys and values" do
-      enigma.to_hash.tap do |h|
-        h.each do |key, value|
-          key.encoding.name.must_equal "US-ASCII"
-          key.valid_encoding?.must_equal true
-          value.encoding.name.must_equal "US-ASCII"
-          value.valid_encoding?.must_equal true
-        end
+      h = enigma.to_hash
+      h.each do |key, value|
+        key.encoding.name.must_equal "US-ASCII"
+        key.valid_encoding?.must_equal true
+        value.encoding.name.must_equal "US-ASCII"
+        value.valid_encoding?.must_equal true
       end
     end
   end
 
   describe ".from_hash" do
-    it "accepts hash with string keys for easy deserialization" do
-      hash = {
+    let(:hash) do
+      {
         "cipher" => "AES-128-TEST",
         "ciphertext" => bin64("test_ct"),
         "encrypted_key" => bin64("test_ek"),
         "iv" => bin64("test_iv")
       }
-      Strongroom::Enigma.from_hash(hash).tap do |e|
-        e.cipher.must_equal "AES-128-TEST"
-        e.ciphertext.must_equal binary("test_ct")
-        e.encrypted_key.must_equal binary("test_ek")
-        e.iv.must_equal binary("test_iv")
-      end
+    end
+    it "accepts hash with string keys for easy deserialization" do
+      e = Strongroom::Enigma.from_hash(hash)
+      e.cipher.must_equal "AES-128-TEST"
+      e.ciphertext.must_equal binary("test_ct")
+      e.encrypted_key.must_equal binary("test_ek")
+      e.iv.must_equal binary("test_iv")
     end
   end
 
@@ -79,7 +78,7 @@ describe Strongroom::Enigma do
     it "is not empty" do
       s.wont_be_empty
     end
-    it "has is valid US-ASCII" do
+    it "is valid US-ASCII" do
       s.encoding.name.must_equal "US-ASCII"
       s.valid_encoding?.must_equal true
     end
@@ -87,12 +86,11 @@ describe Strongroom::Enigma do
 
   describe ".deserialize" do
     it "correctly deserializes output from #serialize" do
-      Strongroom::Enigma.deserialize(enigma.serialize).tap do |e|
-        e.cipher.must_equal "AES-128-TEST"
-        e.ciphertext.must_equal binary("ct")
-        e.encrypted_key.must_equal binary("ek")
-        e.iv.must_equal binary("iv")
-      end
+      e = Strongroom::Enigma.deserialize(enigma.serialize)
+      e.cipher.must_equal "AES-128-TEST"
+      e.ciphertext.must_equal binary("ct")
+      e.encrypted_key.must_equal binary("ek")
+      e.iv.must_equal binary("iv")
     end
   end
 
